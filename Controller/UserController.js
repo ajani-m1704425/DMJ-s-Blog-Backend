72924210c10fe4b9501ddc6118eccdeee456741b
ajani-m1704425 <88562907+ajani-m1704425@users.noreply.github.com>
2024-01-28 18:35:46 +0100

    Authentication updated

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -14,7 +14,7 @@ const signupUser = async (req, res) => {
     try {
         const user = await User.signup(email, password)
         const token = createToken(user._id)
-        res.status(200).json({ email, token })
+        res.status(200).json({ user, token })
     } catch (error) {
         res.status(400).json({error: error.message})
     }
@@ -27,7 +27,7 @@ const loginUser = async  (req, res) => {
     try {
         const user = await User.login(email, password)
         const token = createToken(user._id)
-        res.status(200).json({ email, token })
+        res.status(200).json({ user, token })
     } catch (error) {
         res.status(400).json({error: error.message})
     }
